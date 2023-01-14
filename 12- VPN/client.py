a42835ec5fafcd9524035b9ed1efb55bb0709421
Shebu <67383098+sh3bu@users.noreply.github.com>
2023-01-14 08:09:43 +0530

    Create client.py

---
+++ 12- VPN/client.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import fcntl
+import struct
+import os
+import time
+from scapy.all import *
+
+TUNSETIFF = 0x400454ca
+IFF_TUN   = 0x0001
+IFF_TAP   = 0x0002
+IFF_NO_PI = 0x1000
+
+# Create the tun interface
+tun = os.open("/dev/net/tun", os.O_RDWR)
+ifr = struct.pack('16sH', b'shebu%d', IFF_TUN | IFF_NO_PI)
+ifname_bytes  = fcntl.ioctl(tun, TUNSETIFF, ifr)
+
+# Get the interface name
+ifname = ifname_bytes.decode('UTF-8')[:16].strip("\x00")
+print("Interface Name: {}".format(ifname))
+
+os.system("ip addr add 192.168.53.99/24 dev {}".format(ifname))
+os.system("ip link set dev {} up".format(ifname))
+
+while True:
+        packet = os.read(tun, 2048)
+        if packet:
+                ip = IP(packet)
+                print(ip.summary)
+
+                newip = IP(src='192.168.53.3', dst=ip.src)
+                newpkt = newip/ip.payload
+                arb_data = b'Any arbitrary data'
+                os.write(tun, arb_data)
+
+
+while True:
+   time.sleep(10)
