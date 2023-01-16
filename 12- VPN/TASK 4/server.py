8e81599dce1aebc83f33c374bac1ba74c1ca0ec4
Shebu <67383098+sh3bu@users.noreply.github.com>
2023-01-16 15:46:16 +0530

    Update server.py

--- 12- VPN/TASK 4/server.py
+++ 12- VPN/TASK 4/server.py
@@ -1,11 +1,11 @@
 from scapy.all import*
 TUNSETIFF = 0x400454ca
 IFF_TUN = 0x0001
-IFF_TAP - 0x0002
+IFF_TAP = 0x0002
 IFF_NO_PI = 0x1000
 
 tun = os.open("/dev/net/tun", os.O_RDWR)
-ifr = struct.pacl('16sH', b'shebu%d', IFF_TUN | IFF_NO_PI)
+ifr = struct.pack('16sH', b'shebu%d', IFF_TUN | IFF_NO_PI)
 ifname_bytes = fcntl.ioctl(tun, TUNSETIFF, ifr)
 
 ifname = ifname_bytes.decode('UTF-8')[:16].strip("\x00")
