77335e04e4cabedc37c68ed8b5ec7e0a417f0bcd
Shebu <67383098+sh3bu@users.noreply.github.com>
2023-01-16 15:13:09 +0530

    Create server.py

---
+++ 12- VPN/TASK 4/server.py
@@ -0,0 +1,27 @@
+from scapy.all import*
+TUNSETIFF = 0x400454ca
+IFF_TUN = 0x0001
+IFF_TAP - 0x0002
+IFF_NO_PI = 0x1000
+
+tun = os.open("/dev/net/tun", os.O_RDWR)
+ifr = struct.pacl('16sH', b'shebu%d', IFF_TUN | IFF_NO_PI)
+ifname_bytes = fcntl.ioctl(tun, TUNSETIFF, ifr)
+
+ifname = ifname_bytes.decode('UTF-8')[:16].strip("\x00")
+print("Interface name: {}".format(ifname))
+
+os.system("ip addr add 192.168.53.50/24 dev {}".format(ifname))
+os.system("ip link set dev {} up".format(ifname))
+
+IP_A = "0.0.0.0"
+PORT = 9090
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((IP_A, PORT))
+
+while True:
+        data, (ip, port) =sock.recvfrom(2048)
+        print("{}:{} --> {}:{}".format(ip,port,IP_A,PORT))
+        pky = IP(data)
+        print("Inside : {} --> {}".format(pkt.src,pkt.dst))
+        os.write(tun, bytes(pkt))
